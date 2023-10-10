661f72e9e06d880582e94308b3c3542af16a57be
Vlad <laptew.vlad2016@gmail.com>
2023-10-10 20:44:08 +0400

    dolgo

--- chart.py
+++ chart.py
@@ -24,67 +24,72 @@ def chart(coeff: list, window):
     ht = coeff[4] / K
     s_t = int(coeff[11]) * ht
     s_r = int(coeff[12]) * hr
-    tt = [s_k * ht]
-    rr = [s_i * hr]
-    print("rr", rr)
-    print("tt", tt)
     tr = [i * ht for i in range(K + 1)]
     rt = [i * hr for i in range(I + 1)]
+    tt = tr
+    rr = rt
+    print("Version", mpl.__version__)
+    print("rr", rr)
+    print("tt", tt)
     print("rt", rt)
     print("tr", tr)
-    Vt = []
-    Vr = []
-    if s_k == 0:
-        for i in rt:
-            Vt.append(t0(i, coeff[0]))
-    else:
-        for j in rt:
+    Vt = [[] for i in range(len(tr))]
+    Vr = [[] for i in range(len(rt))]
+    for i in range(len(rt)):
+        Vt[0].append(t0(rt[i], coeff[0]))
+        print("len Vt[0]", len(Vt[0]))
+    for i in range(1, len(tt)):
+        for j in range(len(rt)):
             val = 0
             for k in range(1, len(C1k)):
-                x = j0(C1k[k] * j) * C2k[k] / exp((C3k[k] * s_t))
+                x = j0(C1k[k] * rt[j]) * C2k[k] / exp((C3k[k] * tt[i]))
                 val += x
-            Vt.append(val)
+            Vt[i].append(val)
+    for i in range(1, len(tt)):
         for j in range(len(rt)):
-            Vt[j] += coeff[2] + C1 / exp(C2 * s_t)
+            Vt[i][j] += coeff[2] + C1 / exp(C2 * tt[i])
 
-    for i in rr:
-        Vr.append(t0(i, coeff[0]))
-    for j in range(1, len(tr)):
-        val = 0
-        for k in range(1, len(C1k)):
-            val += j0(C1k[k] * s_r) * C2k[k] / exp((C3k[k] * tr[j]))
-        Vr.append(val)
-    for j in range(1, len(tr)):
-        Vr[j] += coeff[2] + C1 / exp(C2 * tr[j])
+    for i in range(len(rr)):
+        Vr[i].append(t0(rr[i], coeff[0]))
+    for i in range(len(rr)):
+        for j in range(1, len(tr)):
+            val = 0
+            for k in range(1, len(C1k)):
+                val += j0(C1k[k] * rr[i]) * C2k[k] / exp((C3k[k] * tr[j]))
+            Vr[i].append(val)
+    for i in range(len(rr)):
+        for j in range(1, len(tr)):
+            Vr[i][j] += coeff[2] + C1 / exp(C2 * tr[j])
 
     plot(window, Vt, rt, tt, Vr, rr, tr, s_i, s_k, schema_solve)
 
 
-def plot(window, Vt, rt, tt, Vr, rr, tr,s_i, s_k, schema_solve):
+def plot(window, Vt, rt, tt, Vr, rr, tr, s_i, s_k, schema_solve):
 
     fig = Figure(figsize=(10, 5), dpi=100)
     colors = ["red", "blue", "green", "black", "orange", "peru", "aqua", "pink", "olive", "lime"]
     plot1 = fig.add_subplot(1, 2, 1)
     plot2 = fig.add_subplot(1, 2, 2)
-    max_t = max_r = 0
-    for i in range(len(Vt)):
-        temp = abs(Vt[i]-schema_solve[s_k,i])
-        if temp > max_t:
-            max_t = temp
-    for i in range(len(Vr)):
-        temp = abs(Vr[i]-schema_solve[i,s_i])
-        if temp > max_r:
-            max_r = temp
-    plot1.set_title(f'Погрешность = {max_t}')
-    plot2.set_title(f'Погрешность = {max_r}')
+    max_e = 0
+    print("len Vt", len(Vt))
+    print("len Vt[0]", len(Vt[0]))
+    print('size', schema_solve.shape)
+    #print('Vt', Vt)
+    #print('Vr', Vr)
+    for k in range(len(Vt)):
+        for i in range(len(Vt[0])):
+            temp = abs(Vt[k][i]-schema_solve[k, i])
+            if temp > max_e:
+                max_e = temp
+    plot1.set_title(f'Погрешность = {max_e}')
     plot2.set_xlabel('t, с', fontsize=14)
     plot2.set_ylabel('V(r, t), град', fontsize=14)
     plot1.set_xlabel('r, см', fontsize=14)
     plot1.set_ylabel('V(r, t), град', fontsize=14)
-    plot2.plot(tr, Vr, color=colors[0], label=f'r={rr[0]}')
-    plot1.plot(rt, Vt, color=colors[0], label=f't={tt[0]}')
-    plot2.plot(tr, schema_solve[:,s_i], color=colors[2], label=f'r={rr[0]} (S)')
-    plot1.plot(rt, schema_solve[s_k,:], color=colors[2], label=f't={tt[0]} (S)')
+    plot2.plot(tr, Vr[s_i], color=colors[0], label=f'r={rr[s_i]}')
+    plot1.plot(rt, Vt[s_k], color=colors[0], label=f't={tt[s_k]}')
+    plot2.plot(tr, schema_solve[:, s_i], color=colors[2], label=f'r={rr[s_i]} (S)')
+    plot1.plot(rt, schema_solve[s_k, :], color=colors[2], label=f't={tt[s_k]} (S)')
     plot1.legend(fontsize="small")
     plot2.legend(fontsize="small")
 
